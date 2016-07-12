2f4f0ceb7ef379efd12c38bcacc455751ff5886c
Anton <darktitan125@gmail.com>
2016-07-12 22:21:48 +0300

    added horizontal and vertical motion

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 
-var i = 0;
+var x = 4,
+    y = 0,
+    speed = 500,
+    snakeLength = 3;
 setInterval(function() {
-    $('.row-2 .block').eq(i).addClass('gray');
-    $('.block-2').eq(i).addClass('gray');
-    i++;
-},500);
+    //horizontalMotion();
+    verticalMotion();
+
+    function horizontalMotion () {
+        var row = $('.row-' + y + ' .block');
+
+        moving(row, x);
+        console.log('x=' + x);
+        x++;
+        if (x > 10) {
+            x = 0;
+        }
+    }
+
+    function verticalMotion () {
+
+        var col = $('.block-' + x);
+
+        moving(col, y);
+        console.log (y);
+        if (y < 10) {
+            y++;
+        } else {
+            y = 0;
+        }
+    }
+
+    function moving (el, i) {
+        el.eq(i).addClass('gray');
+        el.eq(i - snakeLength).removeClass('gray');
+    }
+}, speed);
 
