eb6b4bfc97fa89eb59af498859d5d135f450ea36
Anton <darktitan125@gmail.com>
2016-07-14 23:05:02 +0300

    upgrade events

--- app.js
+++ app.js
@@ -3,48 +3,45 @@ var x = 4,
     y = 5,
     speed = 400,
     snakeLength = 3;
-setInterval(function() {
 
-    document.addEventListener('keydown', function(e) {
-        if (e.keyCode = 40) {
-            verticalMotion();
-        }
-
-        if (e.keyCode = 39) {
-            horizontalMotion();
-        }
-
-    });
-
-    function horizontalMotion () {
-        var row = $('.row-' + y + ' .block');
-
-        moving(row, x);
-
-        x++;
-        if (x > 9) {
-            x = 0;
-        }
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode = 40) {
+        verticalMotion();
     }
+    //if (e.keyCode = 39) {
+    //    horizontalMotion();
+    //}
+});
 
-    function verticalMotion () {
+function verticalMotion () {
+    var col = $('.block-' + x);
 
-        var col = $('.block-' + x);
-
+    setInterval(function() {
         moving(col, y);
-        console.log (y);
+        //console.log (y);
         if (y < 9) {
             y++;
         } else {
             y = 0;
         }
-    }
+    }, speed);
+}
 
-    function moving (el, i) {
-        el.eq(i).addClass('gray');
-        el.eq(i - snakeLength).removeClass('gray');
-    }
-}, speed);
+function horizontalMotion () {
+    var row = $('.row-' + y + ' .block');
 
+    setInterval(function() {
+        moving(row, x);
+        x++;
+        if (x > 9) {
+            x = 0;
+        }
+    }, speed);
+}
+
+function moving (el, i) {
+    el.eq(i).addClass('gray');
+    el.eq(i - snakeLength).removeClass('gray');
+}
 
 
